cfee263ff4d28a98d7317673063d19d81e7dc302
olessiap <olessia.potapova@gmail.com>
2019-07-04 02:08:55 -0300

    some styling

--- src/Form.js
+++ src/Form.js
@@ -29,30 +29,34 @@ class Form extends Component {
     render() {
         const { neighborhood, name, details } = this.state
         return(
-            <form>
-                <label>neighborhood</label>
-                <input 
-                    type="text"
-                    name="neighborhood"
-                    value={neighborhood}
-                    onChange={this.handleChange}/>
-                <label>Name</label>
-                <input 
-                    type="text"
-                    name="name"
-                    value={name}
-                    onChange={this.handleChange}/>
-                <label>Details</label>
-                <input 
-                    type="text"
-                    name="details"
-                    value={details}
-                    onChange={this.handleChange}/>
-                <input 
-                    type="button"
-                    value="Submit"
-                    onClick={this.submitForm} />
-            </form>
+            <div>
+                <h2>Add New Place</h2>
+                <form>
+                    <label>neighborhood</label>
+                    <input 
+                        type="text"
+                        name="neighborhood"
+                        value={neighborhood}
+                        onChange={this.handleChange}/>
+                    <label>Name</label>
+                    <input 
+                        type="text"
+                        name="name"
+                        value={name}
+                        onChange={this.handleChange}/>
+                    <label>Details</label>
+                    <input 
+                        type="text"
+                        name="details"
+                        value={details}
+                        onChange={this.handleChange}/>
+                    <input 
+                        type="button"
+                        value="Submit"
+                        onClick={this.submitForm} />
+                </form>
+            </div>
+            
         )
     }
 }
